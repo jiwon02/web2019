2ec1d250604ad5be6767603ebd76ce9fdd76cb19
jiwon02 <57124851+jiwon02@users.noreply.github.com>
2019-11-07 17:18:58 +0900

    web

---
+++ php/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta charset="UTF-8">
+	<title>php 연습</title>
+	<style>
+		*{margin: 0;padding: 0;}
+		#header{
+			width: 1000px;height: 200px;
+			background-color: #0f0;
+			margin: 0 auto;
+		}
+		#visual{
+			height: 300px;
+			background-color: #ccc;
+		}
+		#container{
+			width: 1000px;height: 500px;
+			background-color: #ff0;
+			margin: 0 auto;
+		}
+		#footer{
+			width: 1000px;height: 200px;
+			background-color: #000;
+			margin: 0 auto;
+		}
+	</style>
+</head>
+<body>
+	<header id="header">
+		<h1><a href="./">LOGO</a></h1>
+		<p><a href="sub01.php">sub01</a></p>
+	</header>
