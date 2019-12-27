43d2e0afe6a53827b3023cdacb1b8dd7c0e2b529
jiwon02 <57124851+jiwon02@users.noreply.github.com>
2019-12-27 18:24:18 +0900

    web

--- site/portfolio/js/work.js
+++ site/portfolio/js/work.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
+	var $item_arr=[
+		["img_01.jpg", "title_01", "context_01", "https://www.naver.com"],
+		["img_02.jpg", "title_02", "context_02", "https://www.google.com"],
+		["img_03.jpg", "title_03", "context_03", "https://www.daum.net"]
+	];
+	$(".grid-item a").click(function(){
+		var $index=$(this).closest(".grid-item").index();
+		$(".popup_img").css("background-image", "url(img/work/"+$item_arr[$index][0]+")");
+		$(".popup_title").text($item_arr[$index][1]);
+		$(".popup_text").text($item_arr[$index][2]);
+		$(".popup_link a").attr("href", $item_arr[$index][3]);
+		return false;
+	});
+
 	$(".grid").masonry({
 		itemSelector : ".grid-item",  //그리드를 적용할 대상
 		isAnimated : true  //애니메이션 기능 활성화(반응형까지 진행될 경우, 움직이는 동적 효과를 부여할 수 있음)
 	});
-
 
 
 
