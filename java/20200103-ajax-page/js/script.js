d1c06e1f2e98a7b7520ffd259c6b1b22cdae4e2d
jiwon02 <57124851+jiwon02@users.noreply.github.com>
2020-01-03 13:21:41 +0900

    web

---
+++ java/20200103-ajax-page/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+	$.ajax({
+		url:"coverstory.html",
+		type:"post",
+		success:function(result){
+			$("#content").html(result);
+		}
+	});
+
+	$("nav li:nth-child(1)").click(function(){
+		$.ajax({
+			url:"coverstory.html",
+			type:"post",
+			success:function(result){
+				$("#content").html(result);
+			}
+		});
+		$("nav li").removeClass("active");
+		$(this).addClass("active");
+		return false;
+	});
+	
+	$("nav li:nth-child(2)").click(function(){
+		$.ajax({
+			url:"characters.html",
+			type:"post",
+			success:function(result){
+				$("#content").html(result);
+			}
+		});
+		$("nav li").removeClass("active");
+		$(this).addClass("active");
+		return false;
+	});
+
+	$("nav li:nth-child(3)").click(function(){
+		$.ajax({
+			url:"snapshot.html",
+			type:"post",
+			success:function(result){
+				$("#content").html(result);
+			}
+		});
+		$("nav li").removeClass("active");
+		$(this).addClass("active");
+		return false;
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ java/20200103-ajax-page/js/snapshot.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$(".more_frame button").click(function(){
+		$.ajax({
+			url:"snapshot_sub.html",
+			type:"post",
+			success:function(result){
+				$(".sp_second").html(result);
+			}
+		});
+		$(this).closest(".more_frame").hide();
+		$(".sp_second").show();
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
